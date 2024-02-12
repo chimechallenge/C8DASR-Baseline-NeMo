0c1787585a009e14e317606a4a11044a6692b6ba
popcornell <cornellsamuele@gmail.com>
2024-02-12 18:50:29 -0500

    fixed relative path for installation of lm

--- scripts/chime8/installers/install_c8_dependencies.sh
+++ scripts/chime8/installers/install_c8_dependencies.sh
@@ -2,4 +2,4 @@ pip install optuna
 pip install git+https://github.com/chimechallenge/chime-utils
 pip install git+https://github.com/kpu/kenlm
 pip install cmake>=3.18
-sh ./installers/run_install_lm.sh ${PWD}/../../../NeMo
+sh ./installers/run_install_lm.sh ${PWD}/../../../C8DASR-Baseline-NeMo
